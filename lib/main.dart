a7e1ad6f330f501faf8fc64b49f70179d559a9c6
YOAN <yoannikaros29@gmail.com>
2022-04-23 15:00:13 +0700

    koment

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       routes: {
         //membuat route
+        //chjccgcvjvk
         '/': (context) => SplashPage(),
 
       },
