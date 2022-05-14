26b3c9e053fadaec8e7bcab3953e68a22df6832b
YOAN <yoannikaros29@gmail.com>
2022-05-14 09:54:22 +0700

    Add name and profile at home

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,14 +1,100 @@
 import 'package:flutter/material.dart';
 
+import '../theme.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Home Page'
-      ),
+    Widget header() {
+      return Container(
+        margin: EdgeInsets.only(
+            top: defaultMargin, left: defaultMargin, right: defaultMargin),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                //colum dibungkis expanded
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Hello, Yoan',
+                    style: primaryTextStyle.copyWith(
+                        fontSize: 24, fontWeight: semiBold),
+                  ),
+                  Text(
+                    '@Yoannikaros',
+                    style: subtitleTextStyle.copyWith(fontSize: 16),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              width: 54,
+              height: 54,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                      image: AssetImage('aset/image_profile.png'))),
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget categories() {
+      return Container(
+        margin: EdgeInsets.only(top: defaultMargin),
+        child: Row(
+          children: [
+            SizedBox(
+              width: defaultMargin,
+            ),
+
+            //Satu
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+              margin: EdgeInsets.only(right: 16),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                color: primaryColor,
+              ),
+              child: Text(
+                'All Shoes',
+                style:
+                primaryTextStyle.copyWith(fontSize: 13, fontWeight: medium),
+              ),
+            ),
+
+            //DUA
+
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+              margin: EdgeInsets.only(right: 16),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                border: Border.all(
+                    color: subtitleColor
+                ),
+                color: transparentColor,
+              ),
+              child: Text(
+                'Running',
+                style:
+                subtitleTextStyle.copyWith(fontSize: 13, fontWeight: medium),
+              ),
+            ),
+
+          ],
+        ),
+      );
+    }
+
+    return ListView(
+      children: [
+        header(),
+      ],
     );
   }
 }
