5bb570a8d3db28be664545470fe601b56a9c9bd2
YOAN <yoannikaros29@gmail.com>
2022-05-16 11:28:27 +0700

    create news card

---
+++ lib/cardwidget/newscard.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NewsCard extends StatelessWidget {
+  const NewsCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
